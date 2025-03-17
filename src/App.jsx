367f7824a1126cb75f8916c674fe7c396ac5dacc
nlauvray <noalauvray@gmail.com>
2025-03-17 19:07:39 +0100

    fix: add 'no-unused-vars' rule to ESLint config and update import paths in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import ProductPage from "./pages/ProductPage";
-import UserCreate from "./components/UserCreate";
-import UserLogin from "./components/UserLogin";
-import Shop from "./pages/Shop";
-import Cart from "./components/Cart";
-import Error404 from "./pages/Error404";
-import { Box } from "@mui/material";
-import { CartProvider } from "./components/CartContext";
+import { Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import ProductPage from './pages/ProductPage';
+import UserCreate from './pages/UserCreate';
+import UserLogin from './pages/UserLogin';
+import Shop from './pages/Shop';
+import Cart from './pages/Cart';
+import Error404 from './pages/Error404';
+import { Box } from '@mui/material';
+import { CartProvider } from './context/CartContext';
 
 function App() {
   return (
