80c93311aef65eb3bfbdbb517b3632c1034b35f2
nlauvray <noalauvray@gmail.com>
2025-03-18 09:17:49 +0100

    fix_node

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ export default [
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      ...globals.node,
+      globals: globals.node,
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
